461dde90662d24bbc59178a0a4c28ef705d540f4
Papi <romain.rousseau21110@gmail.com>
2025-12-19 14:55:41 +0100

    VFX Manager
    Rocket & Confetti done

---
+++ Assets/Scripts/VFX/VFXManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Gift;
+using TMPro;
+using UI;
+using UnityEngine;
+
+namespace VFX
+{
+    public class VFXManager : MonoBehaviour
+    {
+        [SerializeField] private Bag bag;
+        [SerializeField] private CoinUI coinUI;
+        
+        [field : SerializeField]
+        public ParticleSystem Rocket { get; private set; }
+        
+        [field : SerializeField]
+        public ParticleSystem Confetti { get; private set; }
+
+        private void OnEnable()
+        {
+            bag.OnBagTriggered += PlayConfetti;
+            coinUI.OnNewMaxScore += PlayRocket;
+        }
+
+        private void OnDisable()
+        {
+            bag.OnBagTriggered -= PlayConfetti;
+            coinUI.OnNewMaxScore -= PlayRocket;
+        }
+
+        public void PlayConfetti()
+        {
+            Confetti.Play();
+        }
+
+        public void PlayRocket()
+        {
+            Rocket.Play();
+        }
+    }
+}
